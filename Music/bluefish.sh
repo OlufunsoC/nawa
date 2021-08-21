94a4367f74d82bb0b82d92e37f40ff8273849fa8
Olufunso <olufunsocoker@gmail.com>
2021-08-21 10:14:06 +0100

    doings2

---
+++ Music/bluefish.sh
@@ -0,0 +1,6 @@
+echo Welcome to 2022 
+echo The year you start 2 great new jobs and become rich
+echo If you believe tell me your name
+read response
+echo Oh $response, you are indeed blessed
+echo Today is `date` mark my words
